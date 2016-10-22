d268d841a21d15794ba09b01fd325a83ff6411a0
vincent chen <vincent.yd.chen@gmail.com>
2016-10-22 16:24:17 +0800

    add zshrc install

--- install.sh
+++ install.sh
@@ -13,6 +13,12 @@ function install_tmux_configuration()
   ln -s $ROOTDIR/config/tmux/tmux.conf $HOME/.tmux.conf
 }
 
+function install_zsh_configuration()
+{
+  rm -rf $HOME/.zshrc
+  ln -s $ROOTDIR/config/shell/zsh/zshrc $HOME/.zshrc
+}
 
+install_zsh_configuration
 install_vim_configuration
 install_tmux_configuration
